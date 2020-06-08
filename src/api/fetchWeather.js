6e9a825163211aa00a3c35f00813fd9c8ebbec11
awp <awpxwork@gmail.com>
2020-06-08 22:02:00 +0700

    add finished project files

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'a6623e2348bf2361f416e68946d42b37';
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY
+    } 
+  });
+  return data;
+}
